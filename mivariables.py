b4b0c5095ccee19e6be52046b15577b9adf77826
David-Salazar-1082 <164534046+David-Salazar-1082@users.noreply.github.com>
2024-08-30 12:22:05 -0600

    Add files via upload

---
+++ mivariables.py
@@ -0,0 +1,6 @@
+# variables
+print("Mis variables")
+edad=17
+nombre="David Salazar"
+print("Edad....",edad)
+print("Nombre....",nombre)
---
+++ parafor.py
@@ -0,0 +1,5 @@
+# for en español es para
+print("Ciclo for de repeticion")
+minovias=["Lupita","Ana","Agripiana"]
+for chicas in minovias:
+    print(chicas)
